3ea8ff9840bd36d1a712c7469e4dcfc71fda9c27
Peter Lewis <peterjameslewis4@hotmail.com>
2021-01-15 13:19:42 +0000

    deploy

---
+++ client/build/precache-manifest.ee59b0957d1114074fd0dba1371520c4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49bd45707fe5de3e66737c1ea0329dd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e70ace22b5c77845829",
+    "url": "/static/css/main.f739290c.chunk.css"
+  },
+  {
+    "revision": "634b3216c39b2f38ed3e",
+    "url": "/static/js/2.8c364821.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8c364821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e70ace22b5c77845829",
+    "url": "/static/js/main.febfe39c.chunk.js"
+  },
+  {
+    "revision": "cd77bb1956273d314d45",
+    "url": "/static/js/runtime-main.14680ac4.js"
+  },
+  {
+    "revision": "8f350574df6ee3e00008c72b2bd8d46f",
+    "url": "/static/media/dessert.8f350574.jpg"
+  },
+  {
+    "revision": "a738620349791f87c69232a041426c39",
+    "url": "/static/media/meaty.a7386203.jpg"
+  },
+  {
+    "revision": "25a316ccc2546fe10b2c459003f18f3f",
+    "url": "/static/media/spicy.25a316cc.jpg"
+  },
+  {
+    "revision": "315bd06b43104eba874206f0ba2a5c0a",
+    "url": "/static/media/vegetarian.315bd06b.jpg"
+  }
+]);
